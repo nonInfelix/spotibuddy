d08afc4be8327de163ee19db2b0192761a277af7
Felix Volkmer <noninfelix@gmail.com>
2023-11-26 00:20:00 +0100

    added tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  providers: [AuthService],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'spotify-converter';
+
+  constructor(private auth: AuthService) {}
+
+  login() {
+    // this.auth.getToken().subscribe((token) => {
+    //  this.token = token;});
+    window.location.href = 'http://localhost:3000/auth';
+  }
+  getUser() {
+    this.auth.userInfo();
+  }
 }
---
+++ src/app/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  tokenObj!: {};
+
+  getToken() {
+    return this.http.get('http://localhost:3000/auth');
+  }
+
+  userInfo() {
+    this.http
+      .get('http://localhost:3000/user-profile', { withCredentials: true })
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+}
