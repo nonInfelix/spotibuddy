5a5057d76590e066960261430dfd4f221aa64b51
Felix Volkmer <noninfelix@gmail.com>
2023-12-06 17:10:55 +0100

    fixed timeout

--- src/app/playlist/playlist-confirm/playlist-confirm.component.ts
+++ src/app/playlist/playlist-confirm/playlist-confirm.component.ts
@@ -14,9 +14,7 @@ export class PlaylistConfirmComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    /*
     setTimeout(() => this.router.navigate(['']), 10000);
     setInterval(() => (this.timer -= 1), 1000);
-    */
   }
 }
